f25a0a99a88c1ee492cb53f018d66ba8126b8028
Diego Aldo Burlando <diego@dmm888.com>
2014-12-19 18:38:36 +0100

    Added bootstrap to requirejs setup

--- js/common.js
+++ js/common.js
@@ -1,16 +1,22 @@
 require.config({
 
-    baseurl:"ssssjs/lib",
+    baseurl:"js/lib",
     paths: {
         jquery:"lib/jquery",    
         underscore : "lib/underscore",
+        bootstrap: "lib/bootstrap",
         sub:"app/sub",
         sub1:"app/sub1"       
     },
     shim : {
         underscore : {
             exports : "_"
-        }
+        },
+        bootstrap : { deps :['jquery'] }
     }
+
+
+
+
 });
 
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,4 +1,4 @@
-define(['jquery','underscore','sub',"sub1"], function($,_,sub,sub1) {
+define(['jquery','bootstrap','underscore','sub',"sub1"], function($,bootstrap,_,sub,sub1) {
 	$(document).ready(function() {
      $('#btn').click(function(){
      myFunc();
@@ -12,10 +12,21 @@ define(['jquery','underscore','sub',"sub1"], function($,_,sub,sub1) {
         alert(diego);
     });
 
+$('#btn2').addClass("btn btn-danger");
 		var evens = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
 		alert('Underscore is loaded ->' + evens);
         alert(diego);
         alert(maria);
+
+
+$('#saveall').click(function(){
+        console.log(1);
+        $('#myModal').modal('hide');
+    });
+
+ $("#myModal").modal('show');
+
+
 	});
 
 }); 
